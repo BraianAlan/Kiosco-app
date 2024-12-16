fbad4fa3ebac267053d61b3ef9728f4d31bac02d
Braian Briseña <braian171094@hotmail.com>
2024-12-16 19:08:05 -0300

    agregando .env y .gitignore

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+// Iportar dotenv
+require('dotenv').config();
+
 // Importar Express
 const express = require('express');
 const app = express();
 
 // Configurar el puerto
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Llamo a la base de datos
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  
+  db.connect((err) => {
+    if (err) throw err;
+    console.log('Base de datos conectada');
+  });
+  
 // Ruta inicial
 app.get('/', (req, res) => {
   res.send('¡Hola, Mundo! Tu servidor está funcionando.');
@@ -12,5 +28,11 @@ app.get('/', (req, res) => {
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+
+
+
+
+
+
